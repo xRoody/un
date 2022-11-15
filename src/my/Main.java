e3ce9cb84633c38247af7e435b114b11c0bb428d
xroody <tbn00st@gmail.com>
2022-11-15 20:57:57 +0400

    add hello word

--- src/my/Main.java
+++ src/my/Main.java
@@ -1,4 +1,19 @@
 package my;
 
+import java.util.function.Function;
+
 public class Main {
+    static Integer x=5;
+    public static void main(String[] args) {
+
+        System.out.println("Hello word");
+        System.out.println("Hello user");
+    }
+
+    public static void met(int a){
+        Function<Integer, Integer> b=(n)->{
+            int i = a+1;
+            return n+i;
+        };
+    }
 }
